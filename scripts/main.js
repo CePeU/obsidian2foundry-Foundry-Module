d720f701c337720c5dd66c65160376a1aa7f7d9e
xcepeu <xcepeu@gmail.com>
2026-03-30 12:07:28 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+//This might become code to allow for adjustments of the style via Foundry
+//Maybe also it will become a way to manage the notes in Foundry
+
+Hooks.once('init', () => {
+  console.log('Obsidian2Foundry ready!');
+  // Register custom callouts, Markdown importers, etc.
+});
